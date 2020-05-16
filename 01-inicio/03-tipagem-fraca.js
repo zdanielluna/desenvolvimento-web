e2ed1c57fb8647baa4e2f987c632ae2ffd3d52db
zdanielluna <danzlunaz@gmail.com>
2020-05-16 03:02:25 -0300

    Envia exemplos de tipagem dinamica e seu conceito

---
+++ 01-inicio/03-tipagem-fraca.js
@@ -0,0 +1,14 @@
+/* Tipagem Dinâmica
+    Não é necessário informar o tipo de dado a ser inserido na declaração de uma
+    variável, pois ele define automaticamente de acordo com o tipo de dado 
+    que foi atribuído.  
+*/
+
+let idade
+console.log(typeof idade)
+
+idade = 18
+console.log(typeof idade)
+
+idade = "dezoito"
+console.log(typeof idade)
