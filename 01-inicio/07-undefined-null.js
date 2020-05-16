29c4bbddaa2edd95b738d48e190d3b8c43e040d7
zdanielluna <danzlunaz@gmail.com>
2020-05-16 04:46:34 -0300

    Insere a diferenca entre null e undefined

---
+++ 01-inicio/07-undefined-null.js
@@ -0,0 +1,16 @@
+/* Undefined
+    Valor primitivo, utilizado quando uma variável 
+    não teve valor atribuído. Indica a ausência de um valor qualquer. */
+
+/* Null 
+    Valor primitivo que representa a ausência intencional de 
+    um valor de objeto, não aponta para nenhum endereço de memória. */
+
+// Valor igual
+console.log(null == undefined)
+
+// Tipo diferente
+console.log(null === undefined)
+
+console.log(typeof null)
+console.log(typeof undefined)
