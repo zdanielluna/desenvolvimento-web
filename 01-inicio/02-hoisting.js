b3883698a1619a6ef5e0cc2833dd9de3f6f1398a
zdanielluna <danzlunaz@gmail.com>
2020-05-16 02:56:54 -0300

    Insere o conceito e um exemplo de hoisting

---
+++ 01-inicio/02-hoisting.js
@@ -0,0 +1,8 @@
+/* Hoisting
+    Comportamento padrão que move a declaração da variável
+    var para cima.
+ */
+
+console.log(jogoFavorito)
+var jogoFavorito = "The Witcher 3"
+console.log(jogoFavorito)
