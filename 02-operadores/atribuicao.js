0688bd114a6ed2c38c73cf460e66c960a61a8b74
zdanielluna <danzlunaz@gmail.com>
2020-05-17 04:49:02 -0300

    Inclui exemplos de uso dos operadores de atribuicao

---
+++ 02-operadores/atribuicao.js
@@ -0,0 +1,32 @@
+// São utilizados para atribuir valores as variáveis
+
+let x
+
+// x = 10
+x = 10
+console.log(x)
+
+// x = x+10
+x += 10
+console.log(x)
+
+// x = x-5
+x -= 5
+console.log(x)
+
+// x = x*2
+x *= -2
+console.log(x)
+
+// x = x/15
+x /= 15
+console.log(x)
+
+// x = x%2
+x %= 2
+console.log(x)
+
+// x = x**0 || x = x¹
+x **= 0
+console.log(x)
+
