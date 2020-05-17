3ce349f7b6ddb445c7914b535b49152278a78f71
zdanielluna <danzlunaz@gmail.com>
2020-05-17 04:48:00 -0300

    Exemplifica o uso de operadores aritmeticos

---
+++ 02-operadores/aritmeticos.js
@@ -0,0 +1,35 @@
+/* São utilizados para executar operações aritméticas com números. */
+
+let resultadoSoma = 10 + 15
+console.log(resultadoSoma)
+
+let resultadoSubtracao = 10 - 15
+console.log(resultadoSubtracao)
+
+let resultadoMultiplicacao = 10 * 15
+console.log(resultadoMultiplicacao)
+
+let resultadoDivisao = 10 / 15
+console.log(resultadoDivisao)
+
+let restoDivisao = 10 % 15
+console.log(restoDivisao)
+
+let resultadoExponencial = 5 ** 2
+console.log(resultadoExponencial)
+
+let a = 10
+a++
+console.log(a)
+a--
+console.log(a)
+
+// Resulta em um número
+console.log('18' - 10)
+console.log(20 / '2')
+console.log('10' * '10')
+console.log('Inuyasha' * 10)
+
+// Concatenação
+console.log('30' + 30)
+console.log("40" + "40")
