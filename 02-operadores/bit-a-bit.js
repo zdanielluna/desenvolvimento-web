92bf772b654a3794e79f0504646534b1841d618f
Daniel Luna <danzlunaz@gmail.com>
2020-05-17 12:45:31 -0300

    Adiciona testes com os operadores bit a bit

---
+++ 02-operadores/bit-a-bit.js
@@ -0,0 +1,21 @@
+// Retorna 1 se ambos os bits forem 1
+console.log("and &")
+console.log(0 & 1)
+console.log(1 & 0)
+console.log(1 & 1)
+console.log(0 & 0)
+
+// Retorna 1 se um dos bits for 1
+console.log("or |")
+console.log(0 | 1)
+console.log(1 | 0)
+console.log(1 | 1)
+console.log(0 | 0)
+
+// Retorna 1 se os bits forem diferentes
+console.log("xor ^")
+console.log(0 ^ 1)
+console.log(1 ^ 0)
+console.log(1 ^ 1)
+console.log(0 ^ 0)
+
