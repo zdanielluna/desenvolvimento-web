857cbd000ed810ad7d1b0f1cbea7e3358d82e217
Daniel Luna <danzlunaz@gmail.com>
2020-05-17 08:33:36 -0300

    Insere testes com os operadores de comparacao

---
+++ 02-operadores/comparacao.js
@@ -0,0 +1,27 @@
+// São utilizados para determinar a igualdade ou diferença entre variaveis ou valores
+
+// Maior que
+console.log('18' > 18)
+
+// Maior ou igual
+console.log('18' >= 18)
+
+// Menor que
+console.log('18' < 18)
+
+// Menor ou igual
+console.log('18' <= 18)
+
+// Verifica se são iguais no valor
+console.log('18' == 18)
+
+// Verifica se são iguais no valor e no tipo
+console.log('18' === 18)
+
+// Verifica se são diferentes no valor
+console.log('18' != 18)
+
+// Verifica se são diferentes no valor ou no tipo
+console.log('18' !== 18)
+
+
