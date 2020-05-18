14263a983e4aa367ada993686f504209dfc5dabf
Daniel Luna <danzlunaz@gmail.com>
2020-05-18 11:37:27 -0300

    Adiciona o conceito basico de loop

---
+++ 04-estruturas-repeticao/01-intro-loops.js
@@ -0,0 +1,3 @@
+/* Estruturas de Repetição: 
+    Executam um bloco de códigos até que a condição de parada seja atingida, 
+    ou seja, repede um bloco de códigos quantas vezes forem necessárias. */
