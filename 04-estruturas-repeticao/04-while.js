1e4f426b4887d9fb4247fd8367a217d780ba9ade
Daniel Luna <danzlunaz@gmail.com>
2020-05-18 11:00:39 -0300

    Adiciona exemplos de uso do while

---
+++ 04-estruturas-repeticao/04-while.js
@@ -0,0 +1,14 @@
+/* Pode ser que não ocorra nenhuma repetição se a expressão lógica do while
+   iniciar com o valor false.
+   
+   A repetição ocorre até que a condição de parada seja ativada, ou seja, quando
+   a expressão lógica do while for falsa. */
+
+while(false) {
+  console.log('Esta mensagem não vai ser exibida!')
+}
+
+while (true) {
+  console.log('Eternal Genjutsu')
+}
+
