35fc3ec05ef68df88fdc248d2f70ede3b23e932b
Daniel Luna <danzlunaz@gmail.com>
2020-05-18 09:48:43 -0300

    Envia exemplos utilizando o for of

---
+++ 04-estruturas-repeticao/03-for-of.js
@@ -0,0 +1,16 @@
+/* Percorre os valores do array */
+
+let animesClassicos = [
+  "Dragon Ball Z",
+  "Cavaleiros do Zodiáco",
+  "YuYu Hakusho",
+  "Inuyasha",
+  "Shaman King",
+  "Naruto",
+  "Sailor Moon",
+]
+
+for (valor of animesClassicos) {
+  console.log(valor)
+}
+
