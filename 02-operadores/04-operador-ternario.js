e60ee0a55575386b6ca0ecc7d4a18a16a264740a
Daniel Luna <danzlunaz@gmail.com>
2020-05-18 07:42:08 -0300

    Exemplifica o uso do operador ternario

---
+++ 02-operadores/04-operador-ternario.js
@@ -0,0 +1,9 @@
+/* Atribui valor a uma variável, de acordo com o valor da expressão lógica.
+   O Operador Ternário é representado por (?). */
+
+let infinito = 10 / 0 === Infinity ? "É infinito" : "Não é infinito";
+console.log(infinito)
+
+let idade = 18
+let maiorIdade = idade >= 18 ? "Maior de idade" : "Menor de idade"
+console.log(maiorIdade)
