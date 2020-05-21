26731fb347e75e49ca852a1408183ea9830ec745
Daniel Luna <danzlunaz@gmail.com>
2020-05-21 13:30:01 -0300

    Envia exemplos de arrow function e testa sua forca em relacao a bind()

---
+++ 05-funcoes/03-funcoes-arrow.js
@@ -0,0 +1,51 @@
+/* Motivações:
+    - Reduzir a sintaxe
+    - Impedir a variação do this */
+
+let soma = function (a, b) {
+  return a + b
+}
+
+// Sintaxe básica da arrow function
+soma = (a, b) => {
+  return a + b
+}
+
+// Sintaxe com um parâmetro e retorno implícito
+soma = (a) => a + 0
+
+function helloWorld() {
+  return "helloWorld"
+}
+
+// Sintaxe "sem parâmetros"
+helloWorld = (_) => "Hello World"
+
+helloWorld = () => "Hello World"
+
+// This é "amarrado" ao contexto da arrow function e não varia
+function Pessoa() {
+  this.idade = 0
+
+  setInterval(() => {
+    // A arrow function é invocada por um temporizador
+    this.idade++
+    console.log(this.idade)
+  }, 1000)
+
+  console.log(this === global)
+}
+
+new Pessoa()
+
+// Arrow Function vs Bind Function
+testaContextoThisArrow = (parametro) => console.log(this === parametro)
+
+testaContextoThisArrow(global)
+testaContextoThisArrow(module.exports)
+
+const objetoQualquer = {}
+testaContextoThisArrow(objetoQualquer)
+
+testaContextoThisArrow = testaContextoThisArrow.bind(objetoQualquer)
+testaContextoThisArrow(objetoQualquer) // Arrow Function wins!
