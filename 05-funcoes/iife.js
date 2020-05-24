4f63ddfd26192eede55cd3414a5277bb77c98b2b
Daniel Luna <danzlunaz@gmail.com>
2020-05-23 21:46:49 -0300

    Envia exemplo e conceito de funcao auto invocada

---
+++ 05-funcoes/iife.js
@@ -0,0 +1,7 @@
+/* IIFE -> Immediately Invoked Function Expression */
+
+(function() {
+    console.log('Será executada na hora da sua declaração')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
