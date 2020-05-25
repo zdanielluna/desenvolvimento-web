9328a42729e581792a0d91d29a761105ffa68409
Daniel Luna <danzlunaz@gmail.com>
2020-05-25 00:23:35 -0300

    Atualiza o exemplo inicial

--- 05-funcoes/12-call-apply.js
+++ 05-funcoes/12-call-apply.js
@@ -21,4 +21,5 @@ const carro = { preco: 49990, desc: 0.20 }
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-console.log(get)
+console.log(getPreco.call(carro, 0.17, '$'))
+console.log(getPreco.apply(carro, [0.17, '$']))
