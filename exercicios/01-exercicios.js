f45215d924d2c8072772fbc14e068e5136458e18
Daniel Luna <danzlunaz@gmail.com>
2020-05-25 14:47:15 -0300

    Resolve o primeiro exercicio da lista 1

---
+++ exercicios/01-exercicios.js
@@ -0,0 +1,7 @@
+calculadora = (a, b) =>
+  console.log(
+    `a = ${a} | b = ${b} \na + b = ${a + b} \na - b = ${a - b} \na x b = ${
+      a * b
+    } \na / b = ${(a / b).toFixed(2)}`
+  )
+calculadora(5, 6)
