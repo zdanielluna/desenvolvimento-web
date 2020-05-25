dda0ca6b052dd98b560111656d229b6017e92af3
Daniel Luna <danzlunaz@gmail.com>
2020-05-25 17:43:06 -0300

    Resolve o exercicio 4 da lista 1

---
+++ exercicios/lista01/exercicio04.js
@@ -0,0 +1,8 @@
+imprimeDivisao = (dividendo, divisor) =>
+  console.log(
+    `dividendo ${dividendo} | divisor ${divisor} \nResultado: ${
+      dividendo / divisor
+    } \nResto: ${dividendo % divisor} `
+  )
+
+imprimeDivisao(10, 4)
