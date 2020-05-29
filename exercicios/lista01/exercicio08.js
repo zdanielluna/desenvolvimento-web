fe2b27aa4a352a5cf9ffd26d50c4382e247925c8
Daniel Luna <danzlunaz@gmail.com>
2020-05-28 23:53:45 -0300

    Resolve o exercicio 8 da lista 1

--- exercicios/lista01/exercicio08.js
+++ exercicios/lista01/exercicio08.js
@@ -1,16 +1,23 @@
-let recordeEstatistica = (pontos) => {
-  let maiorPonto = 0
-  let resultado = []
+let pontos = '10, 20, 20, 8, 25, 3, 0, 30, 1'
 
-  pontuacoes = pontos.split(' ')
+let desempenhoJogos = (pontos) => {
+  let pontuacoes = pontos.split(', ')
+  let maiorPonto = pontuacoes[0]
+  let menorPonto = pontuacoes[0]
+  let countRecorde = 0
+  let piorJogo = 0
 
-  for (let i = 0; i < pontuacoes.legth; i ++) {
+  for (let i = 1; i < pontuacoes.length; i++) {
     if (pontuacoes[i] > maiorPonto) {
-        maiorPonto = pontuacoes[i]
-    
+      maiorPonto = pontuacoes[i]
+      countRecorde++
+    } else if (pontuacoes[i] < menorPonto) {
+      menorPonto = pontuacoes[i]
+      piorJogo = i + 1
     }
   }
-  
+
+  return [countRecorde, piorJogo]
 }
 
-console.log(recordeEstatistica('10 20 20 8 25 3 0 30 1'))
+console.log(desempenhoJogos(pontos))
