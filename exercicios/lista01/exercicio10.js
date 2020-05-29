e06ef134d6d3344b67c3ad3e78af3a013ed4befe
Daniel Luna <danzlunaz@gmail.com>
2020-05-28 23:15:43 -0300

    Resolve o exercicio 10 da lista 1

---
+++ exercicios/lista01/exercicio10.js
@@ -0,0 +1,6 @@
+divisivelPorTres = (numero) => (numero % 3 == 0 ? true : false)
+
+console.log(divisivelPorTres(9))
+console.log(divisivelPorTres(10))
+console.log(divisivelPorTres(27))
+console.log(divisivelPorTres(1))
