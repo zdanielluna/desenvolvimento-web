01863ee9cdc044348d8860db29bae0b0ab952b1d
Daniel Luna <danzlunaz@gmail.com>
2020-06-13 18:20:20 -0300

    Resolve o exercicio 33 da lista 1

---
+++ universo-js/js-basico/exercicios/lista01/exercicio33.js
@@ -0,0 +1,8 @@
+let vetorInteiro = [4, 10, 1, -38]
+let vetorString = ['Leite', 'Paçoca', 'Sorvete', 'Melancia']
+let vetorDouble = [4.5, 10.25, 1.75, -38.47]
+
+let novoArray = vetorInteiro.concat(vetorDouble, vetorString)
+// let novoArray = vetorInteiro.concat(vetorDouble).concat(vetorString)
+
+console.log(novoArray)
