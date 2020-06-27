7ff80069f7f0b9ea171aa475106b30d33e78cabb
Daniel Luna <danzlunaz@gmail.com>
2020-06-27 09:46:32 -0300

    Envia um exemplo da estrutura while

---
+++ C-Deitel-Como-Programar/programacao1/while.c
@@ -0,0 +1,24 @@
+/*  While é uma estrutura de repetição, ou seja, repete as instruções dentro de seu bloco de código
+    enquanto a expressão lógica dentro do seu () for verdadeira.
+
+   Testa primeiro a veracidade da expressão lógica, e se ela
+   for inicialmente falsa o bloco de códigos não será executado nenhuma vez.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int count = 0, total = 0, nota = 0, media = 0;
+
+    while (count < 10) {
+        printf("Entre com a nota: \n");
+        scanf("%d", &nota);
+        total += nota;
+        count ++;
+    }
+    
+    media = total/10;
+    printf("A média da turma é: %d\n", media);
+
+    return 0;
+}
