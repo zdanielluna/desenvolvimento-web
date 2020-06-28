3763f6da5c49e38bbb98faa5d22ca9b7e7c09952
Daniel Luna <danzlunaz@gmail.com>
2020-06-28 09:38:59 -0300

    Realiza uma breve introducao ao paradigma OO

---
+++ universo-js/js-basico/06-Objeto/01-intro-objetos.js
@@ -0,0 +1,31 @@
+/* Introdução a Orientação a Objetos */
+
+// GOTO - Utilizado em linguagens não estruturadas, antes do paradigma OO, para realizar "pulos" dentro do fluxo de código 
+//        do programa, sofreu forte crítica.
+
+// O paradigma POO e PROCEDURAL estão incluídas nas linguagens estruturadas.
+
+// Procedural -> Baseado em funções, procedimentos.
+
+// Orientação a Objetos -> Mudança de foco, o objeto é o centro do paradigma, armazenam atributos e comportamentos.
+
+// A ideia da OO é trazer a realidade do mundo abstraída para objetos no seu software.
+
+/* Pilares da OO
+    1- Abstração -> Trazer objetos e entidades do mundo real para dentro do software, sem se preocupar com a complexidade
+                    que esses objetos representam fora do programa, simplificando-os e utilizando apenas o necessário
+                    para o sistema que está sendo desenvolvido.
+
+    2- Encapsulamento -> Esconder as propriedades de um objeto, adicionando mais segurança na aplicação, geralmente
+                         utilizando propriedades privadas. Não é necessário conhecer os 
+                         detalhes de um objeto para utiliza-lo.
+                         
+                         Os detalhes de implementação do sistema são encapsulados, exibindo
+                         para o usuário apenas a interface para utilizar o sistema. 
+
+    3- Herança(Prototype) -> Princípio baseado na relação "É um". Utilizada no reuso de código, 
+                             O objeto filho recebe atributos e comportamentos de um objeto pai.
+
+    4- Polimorfismo -> Conceito de multiplas formas. Alteração do funcionamento interno de um comportamento herdado de um 
+                       objeto pai, de acordo com a necessidade.
+*/
