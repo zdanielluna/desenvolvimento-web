876cdf0eff39f33afb3962157e68940986967268
Daniel Luna <danzlunaz@gmail.com>
2020-06-29 11:47:56 -0300

    Apresenta e utiliza as funcoes de Object

---
+++ universo-js/js-basico/06-Objeto/funcoes-importantes-objetos.js
@@ -0,0 +1,44 @@
+const character = {
+    name: 'Ashen One',
+    soulLevel: 130,
+    convenant: 'Warrior of Sunlight',
+    weapon: 'Sunlight Straight Sword',
+    armor: 'Sunlight Set',
+}
+
+// Object.keys() -> Retorna todas as chaves do objeto passado no parâmetro
+console.log(Object.keys(character))
+
+// Object.values() -> Retorna todos os valores do objeto passado no parâmetro
+console.log(Object.values(character))
+
+// Object.defineProperty() -> Retorna um array que contém subarrays com o par chave valor do objeto
+console.log(Object.entries(character))
+
+// Object.defineProperty() -> Cria atributos de forma mais específica e detalhada
+Object.defineProperty(character, 'soulLevel', {
+    enumerable: false,
+    writable: false,
+    value: 130,
+})
+
+character.soulLevel = 999
+console.log(character.soulLevel)
+console.log(Object.keys(character))
+
+// Object.assign() -> Incrementa ao primeiro objeto passado por parâmetro os atributos dos objetos informados posteriormente
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2)
+console.log(obj)
+
+// Object.freeze() -> Congela o objeto passado por parâmetro, transformando-o em uma constante
+Object.freeze(character)
+character.name = 'Solarie'
+console.log(character.name)
+
+// A função é executada para cada elemento do array retornado por Object.entries()
+Object.entries(character).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
