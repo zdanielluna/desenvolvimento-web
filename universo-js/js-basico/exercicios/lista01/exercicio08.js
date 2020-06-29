899eca38df0b44f0d32d954547bd6a0826859f55
Daniel Luna <danzlunaz@gmail.com>
2020-06-29 18:06:35 -0300

    [CORRECAO] Converte o array de strings para um de inteiros

--- universo-js/js-basico/exercicios/lista01/exercicio08.js
+++ universo-js/js-basico/exercicios/lista01/exercicio08.js
@@ -1,23 +1,23 @@
 let pontos = '10, 20, 20, 8, 25, 3, 0, 30, 1'
 
 let desempenhoJogos = (pontos) => {
-  let pontuacoes = pontos.split(', ')
-  let maiorPonto = pontuacoes[0]
-  let menorPonto = pontuacoes[0]
-  let countRecorde = 0
-  let piorJogo = 0
+    let pontuacoes = pontos.split(', ')
+    let maiorPonto = pontuacoes[0]
+    let menorPonto = pontuacoes[0]
+    let countRecorde = 0
+    let piorJogo = 0
 
-  for (let i = 1; i < pontuacoes.length; i++) {
-    if (pontuacoes[i] > maiorPonto) {
-      maiorPonto = pontuacoes[i]
-      countRecorde++
-    } else if (pontuacoes[i] < menorPonto) {
-      menorPonto = pontuacoes[i]
-      piorJogo = i + 1
+    for (let i = 1; i < pontuacoes.length; i++) {
+        if (parseInt(pontuacoes[i]) > maiorPonto) {
+            maiorPonto = pontuacoes[i]
+            countRecorde++
+        } else if (parseInt(pontuacoes[i]) < menorPonto) {
+            menorPonto = pontuacoes[i]
+            piorJogo = i + 1
+        }
     }
-  }
 
-  return [countRecorde, piorJogo]
+    return [countRecorde, piorJogo]
 }
 
 console.log(desempenhoJogos(pontos))
