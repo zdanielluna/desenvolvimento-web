adb285616fc6ef997b1fae608f78669bbe5a6c07
Daniel Luna <danzlunaz@gmail.com>
2020-07-03 19:53:09 -0300

    Adiciona comentario

--- 07-arrays/05-forEach2-implementacao.js
+++ 07-arrays/05-forEach2-implementacao.js
@@ -2,6 +2,12 @@
 
 const jogos = ['GTAV', 'Dragons Dogma', 'Clash Royale']
 
+/* 1- Cria o atributo forEach2 para o "pai dos arrays."
+   2- O forEach2() é uma função que recebe uma callback, ou seja, uma outra função como parâmetro. 
+   3- A callback é chamada toda vez que o for completa uma volta.
+   4- Os parâmetros da callback são: o valor do array, a posição e o próprio array.
+*/
+
 Array.prototype.forEach2 = function (callback) {
     for (let i = 0; i < this.length; i++) {
         callback(this[i], i, this)
