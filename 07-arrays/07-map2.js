f21ff6b48268bf77c1f5b289f9fb391e336b4f23
Daniel Luna <danzlunaz@gmail.com>
2020-07-05 10:18:55 -0300

    Realiza um desafio utilizando map

---
+++ 07-arrays/07-map2.js
@@ -0,0 +1,9 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}',
+]
+
+const precos = carrinho.map((elemento) => JSON.parse(elemento).preco)
+console.log(precos)
