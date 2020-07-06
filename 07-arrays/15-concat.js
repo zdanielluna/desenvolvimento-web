e2899ff0c9686a96894ddf2d26dc40f2072962ca
Daniel Luna <danzlunaz@gmail.com>
2020-07-06 10:19:19 -0300

    Trabalha com o concat()

---
+++ 07-arrays/15-concat.js
@@ -0,0 +1,9 @@
+const filhas = ['Lara', 'Lana']
+const filhos = ['Daniel', 'Natsu']
+const todos = filhas.concat(filhos, 'Negan')
+
+console.log(filhas)
+console.log(filhos)
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], [5, 6], [[7, 8]]))
