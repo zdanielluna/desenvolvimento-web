333de08fb33d923896bfc31f1ef7071c6d7e27ef
Daniel Luna <danzlunaz@gmail.com>
2020-07-06 19:43:24 -0300

    Estuda o sistema de modulos do nodejs

---
+++ 08-nodejs/01-moduloA.js
@@ -0,0 +1,7 @@
+/* Em Node, um arquivo representa um módulo. O conteúdo de um módulo só está visível para ele mesmo, para que outros 
+   módulos visualizem, é necessário trabalhar com export e import. 
+*/
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ 08-nodejs/01-moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./01-moduloA')
+const moduloB = require('./01-moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB)
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
