bd063c3374f19fa51984dd9b32ba12a201de5de7
Daniel Luna <danzlunaz@gmail.com>
2020-07-07 14:24:13 -0300

    Busca a chinesa com o menor salario

--- 08-nodejs/05-funcionarios/funcionarios.js
+++ 08-nodejs/05-funcionarios/funcionarios.js
@@ -1,7 +1,21 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const mulheres = (funcionario) => funcionario.genero === 'F'
+const chineses = (funcionario) => funcionario.pais === 'China'
+const menorSalario = (funcionarioAnterior, funcionarioAtual) => {
+    return funcionarioAnterior.salario < funcionarioAtual.salario
+        ? funcionarioAnterior
+        : funcionarioAtual
+}
+
 axios.get(url).then((response) => {
     const funcionarios = response.data
-    console.log(funcionarios)
+
+    const funcionario = funcionarios
+        .filter(mulheres)
+        .filter(chineses)
+        .reduce(menorSalario)
+
+    console.log(funcionario)
 })
