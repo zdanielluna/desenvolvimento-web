2871faf7dfaa757979957c8da408d28e65b4fb98
Daniel Luna <danzlunaz@gmail.com>
2020-07-12 19:24:47 -0300

    Atualiza o exemplo

--- 08-nodejs/11-middlewares.js
+++ 08-nodejs/11-middlewares.js
@@ -1,25 +1,25 @@
 // middleware pattern (chain of responsability)
-const passo1 = (contexto, next) => {
-    contexto.valor1 = 'mid1'
+const passo1 = (objeto, next) => {
+    objeto.valor1 = 'mid1'
     next()
 }
 
-const passo2 = (contexto, next) => {
-    contexto.valor2 = 'mid2'
+const passo2 = (objeto, next) => {
+    objeto.valor2 = 'mid2'
     next()
 }
 
-const passo3 = (contexto) => (contexto.valor3 = 'mid3')
+const passo3 = (objeto) => (objeto.valor3 = 'mid3')
 
-const exec = (contexto, ...middlewares) => {
+const exec = (objeto, ...middlewares) => {
     const execPasso = (indice) => {
         middlewares &&
             indice < middlewares.length &&
-            middlewares[indice](contexto, () => execPasso(indice + 1))
+            middlewares[indice](objeto, () => execPasso(indice + 1))
     }
     execPasso(0)
 }
 
-const contexto = {}
-exec(contexto, passo2, passo1, passo3)
-console.log(contexto)
+const objeto = {}
+exec(objeto, passo2, passo1, passo3)
+console.log(objeto)
