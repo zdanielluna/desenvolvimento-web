d8333167bca4a3c39f1c4895620d274baa9d9b69
Daniel Luna <danzlunaz@gmail.com>
2020-07-12 08:33:07 -0300

    Adiciona novos exemplos com o this e o uso da call e apply

--- 08-nodejs/08.this.js
+++ 08-nodejs/08.this.js
@@ -1,15 +1,74 @@
+console.log('Fora de uma função: ')
+console.log(this === module.exports)
+console.log(this === exports)
 console.log(this === global)
 console.log(this === module)
 
-console.log(this === module.exports)
-console.log(this === exports)
-
-function logThis() {
-    console.log('Dentro de uma função')
-    // Dentro de uma função o this não aponta para exports, aponta para o objeto global
-    console.log(this === exports)
+function dentroDeUmaFuncao() {
+    console.log('Dentro de uma função: ')
     console.log(this === module.exports)
     console.log(this === global)
 }
 
-logThis()
+dentroDeUmaFuncao()
+
+// ------------------------------------------------------ // ----------------------------------------------------------------
+
+// Método de um objeto
+const pessoa = {
+    nome: 'Daniel',
+    falarNome() {
+        console.log(this.nome)
+    },
+}
+
+pessoa.falarNome()
+
+// Bind-> O valor de this na função é explicitamente setado
+nome = 'Solarie'
+
+const jogador1 = {
+    nome: 'Shengz',
+}
+
+const inimigo = {
+    nome: 'Sister Freid',
+}
+
+const falarNome = function () {
+    console.log(this.nome)
+}
+
+const falarNomeInimigo = falarNome.bind(inimigo)
+falarNomeInimigo()
+
+const falarNomeJogador1 = falarNome.bind(jogador1)
+falarNomeJogador1()
+
+falarNome()
+
+/* Call e Apply 
+    -> Diferem na passagem de argumentos.
+    -> Seta o contexto do this, assim como o bind(). */
+
+const jogador2 = {
+    nome: 'Lana',
+    ataque: 600,
+    defesa: 500,
+    status() {
+        console.log(
+            `\nNome: ${this.nome} \nAtaque: ${this.ataque} / Defesa: ${this.defesa}`
+        )
+    },
+}
+
+function addAtaqueDef(ataque, defesa) {
+    this.ataque += ataque
+    this.defesa += defesa
+}
+
+jogador2.status()
+addAtaqueDef.call(jogador2, 100, 50)
+jogador2.status()
+addAtaqueDef.apply(jogador2, [50, 25])
+jogador2.status()
