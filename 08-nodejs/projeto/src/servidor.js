73f23e41c7d873fde33484a92e1ec80cdea0c01a
Daniel Luna <danzlunaz@gmail.com>
2020-07-13 20:06:27 -0300

    Atualiza as funcionalidades do servidor

--- 08-nodejs/projeto/src/servidor.js
+++ 08-nodejs/projeto/src/servidor.js
@@ -2,14 +2,22 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./banco-de-dados')
 
-app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
+app.get('/produtos', (req, res) => {
+    res.send(bancoDeDados.getProduto())
 })
 
-app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 4500.0 })
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produtos = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco,
+    })
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
