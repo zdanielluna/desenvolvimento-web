1d6ce516ad34506feb4c2f09a5d7f3ddf3ab24df
zdanielluna <danzlunaz@gmail.com>
2020-07-14 22:25:11 -0300

    Finaliza as funcoes do crud

--- 08-nodejs/projeto/src/banco-de-dados.js
+++ 08-nodejs/projeto/src/banco-de-dados.js
@@ -10,6 +10,7 @@ const produtos = {}
 function salvarProduto(produto) {
     if (!produto.id) produto.id = sequence.id
     produtos[produto.id] = produto
+    return produto
 }
 
 function getProduto(id) {
@@ -20,8 +21,15 @@ function getProdutos() {
     return Object.values(produtos)
 }
 
+function excluirProduto(id) {
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
+}
+
 module.exports = {
     salvarProduto,
     getProduto,
     getProdutos,
+    excluirProduto
 }
