c62a97c539f57f56d6c2f23febe52473780c0df5
zdanielluna <danzlunaz@gmail.com>
2020-07-14 22:24:48 -0300

    Finaliza as middlewares

--- 08-nodejs/projeto/src/servidor.js
+++ 08-nodejs/projeto/src/servidor.js
@@ -1,11 +1,40 @@
+// const porta = 3003
+
+// const express = require('express')
+// const app = express()
+// const bancoDeDados = require('./banco-de-dados')
+
+// app.get('/produtos', (req, res) => {
+//     res.send(bancoDeDados.getProduto())
+// })
+
+// app.get('/produtos/:id', (req, res, next) => {
+//     res.send(bancoDeDados.getProduto(req.params.id))
+// })
+
+// app.post('/produtos', (req, res, next) => {
+//     const produtos = bancoDeDados.salvarProduto({
+//         nome: req.body.name,
+//         preco: req.body.preco,
+//     })
+//     res.send(produto) // JSON
+// })
+
+// app.listen(porta, () => {
+//     console.log(`Servidor executando na porta ${porta}`)
+// })
+
 const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./banco-de-dados')
 
-app.get('/produtos', (req, res) => {
-    res.send(bancoDeDados.getProduto())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
 })
 
 app.get('/produtos/:id', (req, res, next) => {
@@ -13,13 +42,28 @@ app.get('/produtos/:id', (req, res, next) => {
 })
 
 app.post('/produtos', (req, res, next) => {
-    const produtos = bancoDeDados.salvarProduto({
-        nome: req.body.name,
-        preco: req.body.preco,
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
     })
-    res.send(produto) // JSON
+    res.send(produto) // Converte o objeto em JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco 
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}`)
 })
+
