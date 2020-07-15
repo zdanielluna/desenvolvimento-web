5077fb9dfc52304a25291202b44a0fc39e69d532
zdanielluna <danzlunaz@gmail.com>
2020-07-14 22:34:31 -0300

    Atualiza o servidor.js

--- 08-nodejs/projeto/src/servidor.js
+++ 08-nodejs/projeto/src/servidor.js
@@ -1,29 +1,3 @@
-// const porta = 3003
-
-// const express = require('express')
-// const app = express()
-// const bancoDeDados = require('./banco-de-dados')
-
-// app.get('/produtos', (req, res) => {
-//     res.send(bancoDeDados.getProduto())
-// })
-
-// app.get('/produtos/:id', (req, res, next) => {
-//     res.send(bancoDeDados.getProduto(req.params.id))
-// })
-
-// app.post('/produtos', (req, res, next) => {
-//     const produtos = bancoDeDados.salvarProduto({
-//         nome: req.body.name,
-//         preco: req.body.preco,
-//     })
-//     res.send(produto) // JSON
-// })
-
-// app.listen(porta, () => {
-//     console.log(`Servidor executando na porta ${porta}`)
-// })
-
 const porta = 3003
 
 const express = require('express')
