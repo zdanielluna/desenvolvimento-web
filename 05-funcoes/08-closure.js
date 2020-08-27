8ccd82523530b5c20146698ea7967d9afb25ac6b
Daniel Luna <danzlunaz@gmail.com>
2020-08-27 09:52:55 -0300

    Adiciona mais um exemplo de closure

--- 05-funcoes/08-closure.js
+++ 05-funcoes/08-closure.js
@@ -1,15 +1,32 @@
 /* Closure é o escopo criado quando uma função é declarada, este escopo permite a função acessar e manipular
    as variáveis externas a função. */
 
-   const x = 'Global'
+const x = 'Global'
 
-   function out(){
-       const x = 'Local'
-       function inside() {
-          return x 
-       }
-       return inside
-   }
+function out() {
+    const x = 'Local'
+    function inside() {
+        return x
+    }
+    return inside
+}
 
-   const myFunction = out()
-   console.log(myFunction())
+const myFunction = out()
+console.log(myFunction())
+
+// Exemplo 2
+let gols = 1
+
+const time1 = function () {
+    console.log(gols)
+    gols = 2
+}
+
+gols = 3
+
+const time2 = function () {
+    console.log(gols)
+}
+
+time1() // 3
+time2() // 2
